e0cce440c2bd99b5b18a6f02d7b8ac2118c23358
yura702007 <yura702007@gmail.com>
2023-09-07 02:08:18 +0300

    using function re_path

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('cats/<int:cat_id>/', views.categories),
     path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)
 ]
