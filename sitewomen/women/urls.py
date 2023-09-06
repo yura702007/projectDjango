ef562856dae76c4852fafabb12fc2a538dda7c29
yura702007 <yura702007@gmail.com>
2023-09-07 02:21:14 +0300

    registration custom converter

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, re_path
-from . import views
+from django.urls import path, re_path, register_converter
+from . import views, converters
+
+
+register_converter(converters.FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
     path('', views.index),
