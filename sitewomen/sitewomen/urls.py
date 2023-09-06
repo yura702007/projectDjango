04e72a432fcd3cccdd3e98ca5a29e94b65fae67b
yura702007 <yura702007@gmail.com>
2023-09-07 00:23:09 +0300

    redirect paths from women.urls.py

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from women import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('cats/', views.categories),
+    path('', include('women.urls')),
 ]
