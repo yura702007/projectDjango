7cf08ffd5d0475538fd0b021e067c0762f1e7211
yura702007 <yura702007@gmail.com>
2023-09-07 01:29:40 +0300

    view category by convector

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,5 +6,5 @@ def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<p>Это главная страница сайта</p>')
 
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
