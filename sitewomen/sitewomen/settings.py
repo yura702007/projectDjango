c5f8bd546add40a7d3bc33379da5c27521b991a5
yura702007 <yura702007@gmail.com>
2023-09-07 02:43:29 +0300

    adding local host in ALLOWED_HOSTS

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_(6dkkgzn&(qd_bl2g9k%^3jds00g%(#5+4&+3*!5j3fsh*ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
