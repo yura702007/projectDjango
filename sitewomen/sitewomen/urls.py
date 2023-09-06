35580840d4613de4c3f26050f11d8b7d8cad6113
yura702007 <yura702007@gmail.com>
2023-09-07 02:59:24 +0300

    adding import women app

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from women import views
+import women
 
 urlpatterns = [
     path('admin/', admin.site.urls),
