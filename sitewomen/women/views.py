87662b8a52d1ed594f2ebdc486c608534ba875ce
yura702007 <yura702007@gmail.com>
2023-09-07 01:44:16 +0300

    view category by slug-convector

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -8,3 +8,7 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+
+
+def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
