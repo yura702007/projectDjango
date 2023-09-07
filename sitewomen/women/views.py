73695bbb4c733df31f1e3c3fb03f590507a141b0
yura702007 <yura702007@gmail.com>
2023-09-07 03:07:41 +0300

    added checking year in function archive

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -17,6 +17,8 @@ def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
 
 
 def archive(request: HttpRequest, year: int) -> HttpResponse:
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f'<h1>Архив по годам</h1><p>year: {year}</p>')
 
 
