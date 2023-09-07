91dbc143591d0191c672cd0282960edcef494074
yura702007 <yura702007@gmail.com>
2023-09-07 07:05:14 +0300

    path to about view added

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -6,6 +6,7 @@ register_converter(converters.FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('cats/<int:cat_id>/', views.categories, name='cat_id'),
     path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
     path('archive/<yyyy:year>/', views.archive, name='archive')
