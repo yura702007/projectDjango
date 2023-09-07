3042466764a90d0f63bfb93e5e34378ef7a41db2
yura702007 <yura702007@gmail.com>
2023-09-07 03:38:14 +0300

    adding names urls

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -5,8 +5,8 @@ from . import views, converters
 register_converter(converters.FourDigitYearConverter, 'yyyy')
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
-    path('archive/<yyyy:year>/', views.archive)
+    path('', views.index, name='home'),
+    path('cats/<int:cat_id>/', views.categories, name='cat_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path('archive/<yyyy:year>/', views.archive, name='archive')
 ]
