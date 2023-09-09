b31d8d42a1151612bc64573887d1b56050db87bf
yura702007 <yura702007@gmail.com>
2023-09-09 09:20:03 +0300

    DEBUG == True

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u_(6dkkgzn&(qd_bl2g9k%^3jds00g%(#5+4&+3*!5j3fsh*ra'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
