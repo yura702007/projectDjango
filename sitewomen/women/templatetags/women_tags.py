290aa08a345a8adf55f7b53955f78c569125e658
yura702007 <yura702007@gmail.com>
2023-09-17 14:56:54 +0300

    creating inclusion tag

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return views.cats_db
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories():
+    cats = views.cats_db
+    return {'cats': cats}
