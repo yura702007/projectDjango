b329ddcb52e9874cfc89bcdd662c9229663ecc9a
yura702007 <yura702007@gmail.com>
2023-09-19 01:24:42 +0300

    add __str__() method

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -7,3 +7,6 @@ class Women(models.Model):
     time_create = models.TimeField(auto_now_add=True)
     time_update = models.TimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
