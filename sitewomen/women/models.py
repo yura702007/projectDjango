6a858649d944c947f87e4a41feb70c5ab3e0451e
yura702007 <yura702007@gmail.com>
2023-09-23 01:59:44 +0300

    change field slug in model Women for make migration

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    slug = models.SlugField(max_length=255, blank=True, db_index=True, default='')
     content = models.TextField(blank=True)
     time_create = models.TimeField(auto_now_add=True)
     time_update = models.TimeField(auto_now=True)
