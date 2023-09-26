fd67e9363f34374666706f5b1d0d8329db28d241
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-09-26 15:33:53 +0300

    add class Category in models.py

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -33,3 +33,11 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
