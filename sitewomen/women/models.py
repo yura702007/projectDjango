d92bdd51aa6ea9a478f9bb756212f6d0ce18c6b9
yura702007 <yura702007@gmail.com>
2023-09-28 00:19:04 +0300

    add method 'get_absolute_url' in model 'Category'

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -42,3 +42,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
