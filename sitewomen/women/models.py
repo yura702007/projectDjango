90c19189bb2538406d9adf9873a9cad7fc1f841f
yura702007 <yura702007@gmail.com>
2023-09-28 00:43:10 +0300

    update model 'Women'

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -15,8 +15,8 @@ class Women(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
-    time_create = models.TimeField(auto_now_add=True)
-    time_update = models.TimeField(auto_now=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
 
