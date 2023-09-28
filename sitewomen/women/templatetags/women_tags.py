92284491f617f0868b54f389feb18d1d96b5eb59
yura702007 <yura702007@gmail.com>
2023-09-28 03:26:40 +0300

    add function 'show_all_tags' in women_tags.py

--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from .. import views
-from ..models import Category
+from ..models import Category, TagPost
 
 register = template.Library()
 
@@ -9,3 +9,9 @@ register = template.Library()
 def show_categories(cat_selected=0):
     cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
+
+
+@register.inclusion_tag('women/list_tags.html')
+def show_all_tags():
+    tags = TagPost.objects.all()
+    return {'cats': tags}
