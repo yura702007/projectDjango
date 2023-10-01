19475b3f4dbe98aaf40eaadb23c0070056332aa0
yura702007 <yura702007@gmail.com>
2023-10-01 10:44:29 +0300

    add model 'Husband' in models.py

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -37,6 +37,14 @@ class Women(models.Model):
         return reverse('post', kwargs={'post_slug': self.slug})
 
 
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
