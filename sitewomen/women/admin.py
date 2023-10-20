9dc98c47c0901e6772af2951f0ada8f0ca5989d3
yura702007 <yura702007@gmail.com>
2023-10-20 04:01:37 +0300

    edit admin panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Women
+from .models import Women, Category
 
 
 # Register your models here.
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
     list_display_links = ('id', 'title')
+    list_editable = ('is_published', 'cat')
+    list_per_page = 5
     ordering = ['time_create', 'title']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -17,7 +17,9 @@ class Women(models.Model):
     content = models.TextField(blank=True, verbose_name='Статья')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT, verbose_name='Статус')
+    is_published = models.BooleanField(
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT, verbose_name='Статус'
+    )
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
     husband = models.OneToOneField(
@@ -52,8 +54,12 @@ class Husband(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
