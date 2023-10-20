501f13ef6704466ca52ebb8ebf5b62c6f196a1e0
yura702007 <yura702007@gmail.com>
2023-10-20 03:20:43 +0300

    register class WomenAdmin in admin panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Women
 
+
 # Register your models here.
-admin.site.register(Women)
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display_links = ('id', 'title')
+    ordering = ['time_create', 'title']
