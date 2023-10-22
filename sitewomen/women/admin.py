875c7ddd4e34d9f8aa269b2a50ef5c5ac293775c
yura702007 <yura702007@gmail.com>
2023-10-22 12:47:41 +0300

    add custom filter sorting

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from .models import Women, Category
 
 
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Семейное положение'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
@@ -10,7 +27,7 @@ class WomenAdmin(admin.ModelAdmin):
     list_per_page = 5
     ordering = ['time_create', 'title']
     search_fields = ['title__startswith', 'cat__name']
-    list_filter = ['cat__name', 'is_published']
+    list_filter = [MarriedFilter, 'cat__name', 'is_published']
 
 
 @admin.register(Category)
