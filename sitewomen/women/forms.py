470c968721149e58bfb55fc96c7131c539f44ca5
yura702007 <yura702007@gmail.com>
2023-10-24 03:32:26 +0300

    set fields names

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -3,9 +3,9 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label='Заголовок')
+    slug = forms.SlugField(max_length=255, label='URL')
+    content = forms.CharField(widget=forms.Textarea(), required=False, label='Контент')
+    is_published = forms.BooleanField(required=False, label='Статус')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж')
