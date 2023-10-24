8b54ffd6cd7c3b2d0160b40dc0346c3ecc705cff
yura702007 <yura702007@gmail.com>
2023-10-24 04:05:29 +0300

    add attributes to fields form

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -3,9 +3,9 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок')
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
     slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(), required=False, label='Контент')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
     is_published = forms.BooleanField(required=False, label='Статус', initial=True)
     cat = forms.ModelChoiceField(
         queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана'
