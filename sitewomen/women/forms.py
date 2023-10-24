f8f62a5452efbfacf822b0aea59aec5985df7137
yura702007 <yura702007@gmail.com>
2023-10-24 03:38:19 +0300

    set default value True for field is_published

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -6,7 +6,7 @@ class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255, label='Заголовок')
     slug = forms.SlugField(max_length=255, label='URL')
     content = forms.CharField(widget=forms.Textarea(), required=False, label='Контент')
-    is_published = forms.BooleanField(required=False, label='Статус')
+    is_published = forms.BooleanField(required=False, label='Статус', initial=True)
     cat = forms.ModelChoiceField(
         queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана'
     )
