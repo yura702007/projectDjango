bd2b672e69cc93bf157b1eefb8f5590edb8bfd31
yura702007 <yura702007@gmail.com>
2023-10-24 03:12:23 +0300

    set optional fields

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -5,7 +5,7 @@ from .models import Category, Husband
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
     slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea())
-    is_published = forms.BooleanField()
+    content = forms.CharField(widget=forms.Textarea(), required=False)
+    is_published = forms.BooleanField(required=False)
     cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all())
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
