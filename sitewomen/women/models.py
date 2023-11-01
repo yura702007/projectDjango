12a32bce05a9097a8afbd57ba90dbc6aaabebc7b
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-11-01 13:53:46 +0300

    create model upload files

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -94,3 +94,7 @@ class TagPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag', kwargs={'tag_slug': self.slug})
+
+
+class UploadFiles(models.Model):
+    file = models.FileField(upload_to='uploads_model')
