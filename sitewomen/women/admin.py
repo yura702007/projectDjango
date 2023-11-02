18f652efa094cbf56e53b2bcbd2b7f49d901f7c8
yura702007 <yura702007@gmail.com>
2023-11-02 23:04:35 +0300

    add photo in admin-panel

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Women, Category
 
 
@@ -21,16 +23,23 @@ class MarriedFilter(admin.SimpleListFilter):
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    fields = ['title', 'slug', 'cat', 'content', 'husband']
-    # readonly_fields = ['slug']
+    fields = ['title', 'slug', 'cat', 'photo', 'content', 'husband']
+    readonly_fields = ['post_photo']
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
+    list_display = ('id', 'title', 'post_photo', 'time_create', 'is_published', 'cat')
     list_display_links = ('id', 'title')
     list_editable = ('is_published', 'cat')
     list_per_page = 5
     ordering = ['time_create', 'title']
     search_fields = ['title__startswith', 'cat__name']
     list_filter = [MarriedFilter, 'cat__name', 'is_published']
+    save_on_top = True
+
+    @admin.display(description="Изображение")
+    def post_photo(self, women: Women):
+        if women.photo:
+            return mark_safe(f"<img src='{women.photo.url}' width=50>")
+        return "Без фото"
 
 
 @admin.register(Category)
